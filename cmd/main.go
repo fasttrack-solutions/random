1b13447cf084478678a270c4c3c6f705cf0f06fa
FTHans <hans@fasttrack-solutions.com>
2025-03-13 16:13:09 +0100

    initial lib with simple cmd for testing

---
+++ cmd/main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/fasttrack-solutions/random"
+	"github.com/nexidian/gocliselect"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	menu := gocliselect.NewMenu("Select test to run")
+
+	menu.AddItem("Random Uniform Float64 with range (0,1]", "UniformFloat64")
+	menu.AddItem("Random Uniform Int with range (min, max)", "UniformInt64")
+	menu.AddItem("Linear to Exponential transformation", "TransformToExponential")
+	menu.AddItem("Truncate", "Truncate")
+	menu.AddItem("Round", "Round")
+	menu.AddItem("Exit", "Exit")
+
+	choice := menu.Display()
+
+	err := os.MkdirAll("results", 0775)
+	if err != nil {
+		fmt.Println("error creating results directory")
+		return
+	}
+
+	switch choice {
+	case "UniformFloat64":
+		for {
+			res, errPrompt := stringPrompt("how many results do you want to generate?")
+			if errPrompt != nil {
+				fmt.Println("error getting results from prompt:", errPrompt)
+			} else {
+				numbersToGenerate, errAtoi := strconv.Atoi(res)
+				if errAtoi != nil {
+					fmt.Println(res, "is an invalid number, try again")
+				} else {
+					fmt.Print("generating ", res, " random numbers... ")
+
+					fileName, errGenerate := generateUniformFloat64(numbersToGenerate)
+					if errGenerate != nil {
+						fmt.Print("error: ", errGenerate)
+						return
+					}
+
+					fmt.Println("file with results was generated:", fileName)
+
+					break
+				}
+			}
+		}
+
+	case "UniformInt64":
+		for {
+			res, errPrompt := stringPrompt("how many results do you want to generate?")
+			if errPrompt != nil {
+				fmt.Println("error getting results from prompt:", errPrompt)
+			} else {
+				num, errAtoi := strconv.Atoi(res)
+				if errAtoi != nil {
+					fmt.Println(res, "is an invalid number, try again")
+				} else {
+					res, errPrompt = stringPrompt("whats the minimum number?")
+					if errPrompt != nil {
+						fmt.Println("error getting results from prompt:", errPrompt)
+					} else {
+						minimumNumber, errAtoiMin := strconv.Atoi(res)
+						if errAtoiMin != nil {
+							fmt.Println(res, "is an invalid number, try again")
+						} else if minimumNumber < 0 {
+							fmt.Println(res, "is less than 0, try again")
+						} else {
+							res, errPrompt = stringPrompt("whats the maximum number?")
+							if errPrompt != nil {
+								fmt.Println("error getting results from prompt:", errPrompt)
+							} else {
+								maximumNumber, errAtoiMax := strconv.Atoi(res)
+								if errAtoiMax != nil {
+									fmt.Println(res, "is an invalid number, try again")
+								} else if maximumNumber <= minimumNumber {
+									fmt.Println("maximum cannot be less than or equal to minimum number, try again")
+								} else {
+
+									fmt.Print("generating ", res, " random numbers between ", minimumNumber, " and ", maximumNumber, "... ")
+
+									fileName, errGenerate := generateUniformInt64(num, int64(minimumNumber), int64(maximumNumber))
+									if errGenerate != nil {
+										fmt.Print("error: ", errGenerate)
+										return
+									}
+
+									fmt.Println("file with results was generated:", fileName)
+
+									break
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+	case "Exit":
+		return
+	}
+
+	// generateRandom(100)
+}
+
+func stringPrompt(label string) (string, error) {
+	var s string
+	r := bufio.NewReader(os.Stdin)
+	for {
+		_, err := fmt.Fprint(os.Stderr, label+" ")
+		if err != nil {
+			return "", err
+		}
+		s, _ = r.ReadString('\n')
+		if s != "" {
+			break
+		}
+	}
+	return strings.TrimSpace(s), nil
+}
+
+func generateUniformFloat64(numbersToGenerate int) (string, error) {
+	fileName := fmt.Sprintf("results/UniformFloat64-%v.csv", time.Now().UnixMilli())
+
+	f, err := os.Create(fileName)
+	if err != nil {
+		return "", err
+	}
+
+	defer f.Close()
+
+	for i := 0; i < numbersToGenerate; i++ {
+		rnd, errRnr := random.UniformFloat64()
+		if errRnr != nil {
+			return "", errRnr
+		}
+
+		_, errWriteString := f.WriteString(fmt.Sprintf("%v\n", rnd))
+		if errWriteString != nil {
+			return "", errWriteString
+		}
+	}
+
+	return fileName, nil
+}
+
+func generateUniformInt64(numbersToGenerate int, min int64, max int64) (string, error) {
+	fileName := fmt.Sprintf("results/UniformInt64-%v.csv", time.Now().UnixMilli())
+
+	f, err := os.Create(fileName)
+	if err != nil {
+		return "", err
+	}
+
+	defer f.Close()
+
+	for i := 0; i < numbersToGenerate; i++ {
+		rnd, errRnr := random.UniformInt64(min, max)
+		if errRnr != nil {
+			return "", errRnr
+		}
+
+		_, errWriteString := f.WriteString(fmt.Sprintf("%v\n", rnd))
+		if errWriteString != nil {
+			return "", errWriteString
+		}
+	}
+
+	return fileName, nil
+}
---
+++ random.go
@@ -0,0 +1,64 @@
+package random
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+// UniformInt64 generates an int which is greater than or equal to min and less than max using a uniform distribution
+func UniformInt64(min int64, max int64) (int64, error) {
+	num := max - min + 1
+	res, err := rand.Int(rand.Reader, big.NewInt(int64(num)))
+	if err != nil {
+		return 0, err
+	}
+
+	return res.Int64() + min, nil
+}
+
+func UniformFloat64() (float64, error) {
+	var b [8]byte
+	_, err := rand.Read(b[:])
+	if err != nil {
+		return 0, fmt.Errorf("failed to generate secure random number: %s", err.Error())
+	}
+	r := float64(binary.LittleEndian.Uint64(b[:])) / (1 << 64)
+
+	return Truncate(r, 9), nil
+}
+
+// TransformToExponential transforms a number between (0,1) using exponential distribution
+func TransformToExponential(aggression float64, progression float64) (float64, error) {
+	if aggression < 5 || aggression > 100 {
+		return 0, fmt.Errorf("aggression must be between 5 and 100")
+	} else if progression <= 0 {
+		return 0, nil
+	} else if progression >= 1 {
+		return 1, nil
+	}
+
+	expProgression := math.Pow(math.E, aggression*(progression-1))
+	truncated := Truncate(expProgression, 9)
+
+	if truncated == 0 {
+		return 0.000000001, nil
+	}
+
+	return truncated, nil
+}
+
+// Truncate chops off decimals after precision.
+// i.e. Truncate(1.2345678, 5) returns 1.23456 instead of a rounded 1.23457
+func Truncate(val float64, precision int) float64 {
+	multiplier := math.Pow(10, float64(precision))
+	return math.Floor(val*multiplier) / multiplier
+}
+
+// Round function to round a float to a specific number of decimal places
+func Round(val float64, precision int) float64 {
+	multiplier := math.Pow(10, float64(precision))
+	return math.Round(val*multiplier) / multiplier
+}
